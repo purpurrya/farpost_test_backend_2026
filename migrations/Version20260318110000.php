de91ecb21d17d2760878b82f7f18b2890d83f919
purpurrya <anastasia.tikhonenko.meow@gmail.com>
2026-03-23 12:23:55 +1000

    fix dockerfile

--- migrations/Version20260318110000.php
+++ migrations/Version20260318110000.php
@@ -17,13 +17,13 @@ final class Version20260318110000 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE process RENAME INDEX idx_process_machine TO IDX_861D1896F6B75B26');
+        $this->addSql('ALTER TABLE process RENAME INDEX IDX_PROCESS_MACHINE TO IDX_861D1896F6B75B26');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('DROP TABLE messenger_messages');
-        $this->addSql('ALTER TABLE process RENAME INDEX IDX_861D1896F6B75B26 TO idx_process_machine');
+        $this->addSql('ALTER TABLE process RENAME INDEX IDX_861D1896F6B75B26 TO IDX_PROCESS_MACHINE');
     }
 }
 
